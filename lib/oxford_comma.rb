a2b6088a3c23743be9610364e50ce7f0184ef796
Marc Mills <mdmills1990@gmail.com>
2018-08-20 21:36:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,13 +2,13 @@ def oxford_comma(array)
 # if array is only 1 thing, nothing changes
 # elsif array is 2 things, add an "and" between the two objects and turn it into a strings
 # elsif array is 3 or more things, add commas between each object (there was that join(",") method) but put an "and" before the last word
-original_array = array
   if array.length <= 1
     array.join
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    origina_array.pop
-    original_array.join(", ") << ", and #{array.last}"
+    unchanged_array = array
+    array.pop
+    array.join(", ") << ", and #{unchanged_array.last}"
 end
 end
