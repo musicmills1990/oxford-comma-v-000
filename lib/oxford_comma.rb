92ce5ed155ff9b040dac1c3546a6e7dcfbcb81f1
Marc Mills <mdmills1990@gmail.com>
2018-08-20 21:45:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    new_array = array.dup
+    array.pop
+    array.join(", ") << ", and #{new_array.last}"
+end
 end
